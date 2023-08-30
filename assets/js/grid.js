0f1af9cf47db075475eb8c94ff827b7f3d7f91ce
Hamed.H <Hamedhssnzadh@gmail.com>
2023-08-30 13:25:31 +0330

    write functionality to side bar dropdown links

--- assets/js/grid.js
+++ assets/js/grid.js
@@ -48,13 +48,34 @@ $(document).ready(function(){
    },300);
    });
 
-    $('#header-notification-toggle').click(function () {
-            $('#header-notification').fadeToggle()
-    })
-    $('#header-comment-toggle').click(function () {
-        $('#header-comment').fadeToggle()
-    })
-    $('#header-profile-toggle').click(function () {
-        $('#header-profile').fadeToggle()
-    })
+   $('#header-notification-toggle').click(function(){
+      $('#header-notification').fadeToggle();
+   });
+
+   $('#header-comment-toggle').click(function(){
+      $('#header-comment').fadeToggle();
+   });
+
+   $('#header-profile-toggle').click(function(){
+      $('#header-profile').fadeToggle();
+   });
+
+
+
+    $('.sidebar-group-link').click(function() {
+        if ($(this).hasClass('sidebar-group-link-active')) { // Check if clicked link is already active
+            $(this).removeClass('sidebar-group-link-active');
+            $(this).children('.sidebar-dropdown-toggle').children('.angle').removeClass('fa-angle-down');
+            $(this).children('.sidebar-dropdown-toggle').children('.angle').addClass('fa-angle-left');
+        } else {
+            $('.sidebar-group-link').removeClass('sidebar-group-link-active');
+            $('.sidebar-group-link').children('.sidebar-dropdown-toggle').children('.angle').removeClass('fa-angle-down');
+            $('.sidebar-group-link').children('.sidebar-dropdown-toggle').children('.angle').addClass('fa-angle-left');
+
+            $(this).addClass('sidebar-group-link-active');
+            $(this).children('.sidebar-dropdown-toggle').children('.angle').removeClass('fa-angle-left');
+            $(this).children('.sidebar-dropdown-toggle').children('.angle').addClass('fa-angle-down');
+        }
+    });
+
 })
