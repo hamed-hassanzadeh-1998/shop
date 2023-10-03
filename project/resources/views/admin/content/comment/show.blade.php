12a39b9b1da010a657d8da315087797cc867597d
Hamed.H <hamedhssnzdh@gmail.com>
2023-10-03 20:20:08 +0330

    create category and cpmment and faq controller pages

--- project/resources/views/admin/content/comment/show.blade.php
+++ project/resources/views/admin/content/comment/show.blade.php
@@ -1,13 +1,13 @@
 @extends('admin.layouts.master')
 @section('head-tag')
-    <title>نمایش نظرات</title>
+    <title>نمایش نظر</title>
 @endsection
 @section('content')
 
 <nav aria-label="breadcrumb">
     <ol class="breadcrumb">
       <li class="breadcrumb-item font-size-12"> <a href="#"> خانه</a></li>
-      <li class="breadcrumb-item font-size-12"> <a href="#"> بخش فروش</a></li>
+      <li class="breadcrumb-item font-size-12"> <a href="#"> بخش محتوی</a></li>
       <li class="breadcrumb-item font-size-12"> <a href="#"> نظرات</a></li>
       <li class="breadcrumb-item font-size-12 active" aria-current="page"> نمایش نظر ها</li>
     </ol>
@@ -24,7 +24,7 @@
             </section>
 
             <section class="d-flex justify-content-between align-items-center mt-4 mb-3 border-bottom pb-2">
-                <a href="{{ route('admin.market.comment.index') }}" class="btn btn-info btn-sm">بازگشت</a>
+                <a href="{{ route('admin.content.comment.index') }}" class="btn btn-info btn-sm">بازگشت</a>
             </section>
 
             <section class="card mb-3">
