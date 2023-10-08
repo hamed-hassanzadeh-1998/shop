09465212feb88857f199da7fc1ebf365d02edaa1
Hamed.H <Hamedhssnzadh@gmail.com>
2023-10-08 11:32:29 +0330

    install jetstream livewire in project

--- project/database/migrations/2014_10_12_000000_create_users_table.php
+++ project/database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,28 +4,31 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('email')->unique()->nullable();
             $table->string('mobile')->unique()->nullable();
+            $table->string('password');
             $table->string('national_code')->unique()->nullable();
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
             $table->string('slug')->unique()->nullable();
             $table->text('profile_photo_path')->nullable()->comment('avatar');
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
             $table->tinyInteger('activation')->default(0)->comment('0 => inactive, 1 => active');
             $table->timestamp('activation_date')->nullable();
             $table->tinyInteger('user_type')->default(0)->comment('0 => user, 1 => admin');
-            $table->tinyInteger('status')->default(0)->comment('0 => inactive, 1 => active');
+            $table->tinyInteger('status')->default(0);
+            $table->foreignId('current_team_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
@@ -34,9 +37,11 @@ return new class extends Migration
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('users');
     }
-};
+}
