581db735880419abaf616493dff8c68b3ba23f94
Hamed.H <Hamedhssnzadh@gmail.com>
2023-10-14 12:13:42 +0330

    Revert "install jetstream"
    This reverts commit e15f2efcad5dd3115d578a9170442b1e72da388f.

--- project/routes/web.php
+++ project/routes/web.php
@@ -294,13 +294,3 @@ Route::prefix('admin')->namespace('Admin')->group(function () {
         });
     });
 });
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
