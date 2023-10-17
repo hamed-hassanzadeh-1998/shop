ded2210ed4957a70e68491a33c968369917ccab8
Hamed Hassanzadeh <hamedhssnzdh@gmail.com>
2023-10-17 12:10:00 +0330

    Delete project-backup directory

--- project-backup/storage/framework/views/9a88e006204ba65bdac2c5bda2a68d55.php
+++
@@ -1,6 +0,0 @@
-
-<script src="<?php echo e(asset('admin-assets/js/jquery-3.5.1.min.js')); ?>"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="<?php echo e(asset('admin-assets/js/bootstrap/bootstrap.min.js')); ?>"></script>
-<script src="<?php echo e(asset('admin-assets/js/grid.js')); ?>"></script>
-<?php /**PATH C:\Users\hamed.h\Desktop\admin panel\project\resources\views/admin/layouts/scripts.blade.php ENDPATH**/ ?>
--- project-backup/storage/framework/views/d40cb21f3ca2bd505efcbe26c54603fb.php
+++
@@ -1,9 +0,0 @@
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="<?php echo e(asset('admin-assets/css/bootstrap/bootstrap.min.css')); ?>">
-<link rel="stylesheet" href="<?php echo e(asset('admin-assets/fontawsome/css/all.min.css')); ?>">
-<link rel="stylesheet" href="<?php echo e(asset('admin-assets/css/animate.min.css')); ?>">
-<link rel="stylesheet" href="<?php echo e(asset('admin-assets/css/grid.css')); ?>">
-<link rel="stylesheet" href="<?php echo e(asset('admin-assets/css/style.css')); ?>">
-
-<?php /**PATH C:\Users\hamed.h\Desktop\admin panel\project\resources\views/admin/layouts/head-tag.blade.php ENDPATH**/ ?>
