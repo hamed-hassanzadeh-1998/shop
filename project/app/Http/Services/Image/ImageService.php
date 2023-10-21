211e419d82c39aa63e99106ec81159a940eb0260
Hamed.H <hamedhssnzdh@gmail.com>
2023-10-21 17:40:06 +0330

    write function for delete files in Image service

--- project/app/Http/Services/Image/ImageService.php
+++ project/app/Http/Services/Image/ImageService.php
@@ -52,21 +52,21 @@ class ImageService extends ImageToolsService
         //set directory
             $this->getImageDirectory() ??
             $this->setImageDirectory(date("Y")
-                 .DIRECTORY_SEPARATOR
-                 .date('m')
-                 .DIRECTORY_SEPARATOR
-                 .date('d'));
-            $this->setImageDirectory($this->getImageDirectory()
-            .DIRECTORY_SEPARATOR
-            .time());
+                . DIRECTORY_SEPARATOR
+                . date('m')
+                . DIRECTORY_SEPARATOR
+                . date('d'));
+        $this->setImageDirectory($this->getImageDirectory()
+            . DIRECTORY_SEPARATOR
+            . time());
 
-            //set name
-        $this->getImageName()??$this->setImageName(time());
-        $imageName=$this->getImageName();
-        $indexArray=[];
-        foreach ($imageSizes as $sizeAlias=>$imageSize){
+        //set name
+            $this->getImageName() ?? $this->setImageName(time());
+        $imageName = $this->getImageName();
+        $indexArray = [];
+        foreach ($imageSizes as $sizeAlias => $imageSize) {
             // create and set this size name
-            $currentImageName=$imageName.'_'.$sizeAlias;
+            $currentImageName = $imageName . '_' . $sizeAlias;
             $this->setImageName($currentImageName);
 
             //execute provider
@@ -74,23 +74,52 @@ class ImageService extends ImageToolsService
 
             //save image
             $result = Image::make($image->getRealPath())
-                ->fit($imageSize['width'],$imageSize['height'])
+                ->fit($imageSize['width'], $imageSize['height'])
                 ->save(
                     path: public_path($this->getImageAddress()),
                     quality: null,
                     format: $this->getImageFormat());
 
-           if ($result)
-               $indexArray[$sizeAlias]=$this->getImageAddress();
-           else
-               return false;
+            if ($result)
+                $indexArray[$sizeAlias] = $this->getImageAddress();
+            else
+                return false;
 
         }
-        $image['indexArray']=$indexArray;
-        $image['directory']=$this->finalImageDirectory;
-        $image['currentImage']=\Config::get('image.default-current-index-image');
+        $image['indexArray'] = $indexArray;
+        $image['directory'] = $this->finalImageDirectory;
+        $image['currentImage'] = \Config::get('image.default-current-index-image');
 
         return $image;
 
+    }
+
+    public function deleteImage($imagePath): void
+    {
+        if (file_exists($imagePath))
+            unlink($imagePath);
+    }
+
+    public function deleteIndex($image): void
+    {
+        $directory = public_path($image['directory']);
+        $this->deleteDirectoryAndFiles($directory);
+    }
+
+    public function deleteDirectoryAndFiles($directory): bool
+    {
+        if (!is_dir($directory))
+            return false;
+
+
+        $files = glob($directory . DIRECTORY_SEPARATOR . '*', GLOB_MARK);
+
+        foreach ($files as $file) {
+            if (is_dir($file))
+                $this->deleteDirectoryAndFiles($file);
+            else
+                unlink($file);
+        }
+        return rmdir($directory);
     }
 }
