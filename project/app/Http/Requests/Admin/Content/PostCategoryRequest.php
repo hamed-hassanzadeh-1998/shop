c3f9546940099af03c83fb08b69c7e210e86cbbd
Hamed.H <hamedhssnzdh@gmail.com>
2023-10-23 12:41:02 +0330

    add a validation for posts and use regex for validation

--- project/app/Http/Requests/Admin/Content/PostCategoryRequest.php
+++ project/app/Http/Requests/Admin/Content/PostCategoryRequest.php
@@ -21,24 +21,24 @@ class PostCategoryRequest extends FormRequest
      */
     public function rules(): array
     {
-        if ($this->isMethod('post')){
+        if($this->isMethod('post')){
             return [
-               'name'=>'required|max:120|min:2',
-               'description'=>'required|max:500|min:10',
-               'slug'=>'nullable',
-               'image'=>'required',
-               'status'=>'required|numeric|in:0,1',
-               'tags'=>'required',
-            ];
-        }else{
-            return [
-                'name'=>'required|max:120|min:2',
-                'description'=>'required|max:500|min:10',
-                'slug'=>'nullable',
-                'image'=>'nullable',
-                'status'=>'required|numeric|in:0,1',
-                'tags'=>'required',
+                'name' => 'required|max:120|min:2|regex:/^[ا-یa-zA-Z0-9\-۰-۹ء-ي., ]+$/u',
+                'description' => 'required|max:500|min:5|regex:/^[ا-یa-zA-Z0-9\-۰-۹ء-ي.,><\/;\n\r& ]+$/u',
+                'image' => 'required|image|mimes:png,jpg,jpeg,gif',
+                'status' => 'required|numeric|in:0,1',
+                'tags' => 'required|regex:/^[ا-یa-zA-Z0-9\-۰-۹ء-ي., ]+$/u',
             ];
         }
+        else{
+            return [
+                'name' => 'required|max:120|min:2|regex:/^[ا-یa-zA-Z0-9\-۰-۹ء-ي., ]+$/u',
+                'description' => 'required|max:500|min:5|regex:/^[ا-یa-zA-Z0-9\-۰-۹ء-ي.,><\/;\n\r& ]+$/u',
+                'image' => 'image|mimes:png,jpg,jpeg,gif',
+                'status' => 'required|numeric|in:0,1',
+                'tags' => 'required|regex:/^[ا-یa-zA-Z0-9\-۰-۹ء-ي., ]+$/u',
+            ];
+        }
+
     }
 }
