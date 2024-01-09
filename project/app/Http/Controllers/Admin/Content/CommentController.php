852b91dad8d6e62adb34485e2e045e0d20abdd60
Hamed.H <hamedhssnzdh@gmail.com>
2024-01-09 14:14:04 +0330

    write codes for product comments

--- project/app/Http/Controllers/Admin/Content/CommentController.php
+++ project/app/Http/Controllers/Admin/Content/CommentController.php
@@ -14,15 +14,18 @@ class CommentController extends Controller
      */
     public function index()
     {
-        $unSeenComments=Comment::where('seen',0)->get();
+        $unSeenComments =
+            Comment::where('commentable_type', 'App/Models/Content/Post')
+                ->where('seen', 0)->get();
         foreach ($unSeenComments as $unSeenComment) {
-            $unSeenComment->seen=1;
-            $result=$unSeenComment->save();
+            $unSeenComment->seen = 1;
+            $result = $unSeenComment->save();
         }
-       $comments= Comment::query()
+        $comments = Comment::query()
             ->orderBy('created_at', 'desc')
+            ->where('commentable_type', 'App/Models/Content/Post')
             ->simplePaginate(10);;
-     return view('admin.content.comment.index',compact('comments'));
+        return view('admin.content.comment.index', compact('comments'));
     }
 
     /**
@@ -30,7 +33,7 @@ class CommentController extends Controller
      */
     public function show(Comment $comment)
     {
-        return view('admin.content.comment.show',compact('comment'));
+        return view('admin.content.comment.show', compact('comment'));
     }
 
 
@@ -62,25 +65,25 @@ class CommentController extends Controller
         $comment->approved = $comment->approved === 0 ? 1 : 0;
         $result = $comment->save();
         if ($result) {
-         return  redirect()->route('admin.content.comment.index')->with('swal-success','وضعیت نظر با موفقیت تغییر کرد.');
+            return redirect()->route('admin.content.comment.index')->with('swal-success', 'وضعیت نظر با موفقیت تغییر کرد.');
         } else
-        return  redirect()->route('admin.content.comment.index')->with('swal-error','وضعیت نظر با خطا مواجه شد.');
+            return redirect()->route('admin.content.comment.index')->with('swal-error', 'وضعیت نظر با خطا مواجه شد.');
     }
 
-    public function answer(CommentRequest $request,Comment $comment)
+    public function answer(CommentRequest $request, Comment $comment)
     {
-        if ($comment->parent_id == null){
-        $inputs=$request->all();
-        $inputs['author_id']=1;
-        $inputs['parent_id']=$comment->id;
-        $inputs['commentable_id']=$comment->commentable_id;
-        $inputs['commentable_type']=$comment->commentable_type;
-        $inputs['approved']=1;
-        $inputs['status']=1;
-        $comment=Comment::create($inputs);
-        return redirect()->route('admin.content.comment.index')->with('swal-success','پاسخ شما با موفقیت ثبت شد');
-        }else{
-            return redirect()->route('admin.content.comment.index')->with('swal-error','خطا');
+        if ($comment->parent_id == null) {
+            $inputs = $request->all();
+            $inputs['author_id'] = 1;
+            $inputs['parent_id'] = $comment->id;
+            $inputs['commentable_id'] = $comment->commentable_id;
+            $inputs['commentable_type'] = $comment->commentable_type;
+            $inputs['approved'] = 1;
+            $inputs['status'] = 1;
+            $comment = Comment::create($inputs);
+            return redirect()->route('admin.content.comment.index')->with('swal-success', 'پاسخ شما با موفقیت ثبت شد');
+        } else {
+            return redirect()->route('admin.content.comment.index')->with('swal-error', 'خطا');
         }
     }
 }
