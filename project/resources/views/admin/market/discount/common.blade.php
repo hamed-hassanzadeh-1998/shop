b850a52deb3ca3906379a226ce322aa228cc0fc5
Hamed.H <hamedhssnzdh@gmail.com>
2024-01-29 12:39:21 +0330

    write common discounts codes

--- project/resources/views/admin/market/discount/common.blade.php
+++ project/resources/views/admin/market/discount/common.blade.php
@@ -5,13 +5,15 @@
 @endsection
 
 @section('content')
+
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
             <li class="breadcrumb-item font-size-12"> <a href="#">خانه</a></li>
             <li class="breadcrumb-item font-size-12"> <a href="#">بخش فروش</a></li>
-            <li class="breadcrumb-item active font-size-12" aria-current="page"> تخفیف عمومی </li>
+            <li class="breadcrumb-item font-size-12 active" aria-current="page">تخفیف عمومی</li>
         </ol>
     </nav>
+
 
     <section class="row">
         <section class="col-12">
@@ -20,46 +22,64 @@
                     <h5>
                         تخفیف عمومی
                     </h5>
-                    <section class="d-flex justify-content-between align-items-center mt-4 mb-3 border-bottom pb-2">
-                        <a href="{{route('admin.market.discount.commonDiscount.create')}}" class="btn btn-info btn-sm rounded">ایجاد تخفیف عمومی جدید</a>
-                        <div class="max-width-16-rem">
-                            <input class="form-control form-text form-control-sm" type="text" placeholder="جستجو...">
-                        </div>
-                    </section>
-                    <section class="table-responsive">
-                        <table class="table table-striped table-hover">
-                            <thead>
+                </section>
+
+                <section class="d-flex justify-content-between align-items-center mt-4 mb-3 border-bottom pb-2">
+                    <a href="{{ route('admin.market.discount.commonDiscount.create') }}" class="btn btn-info btn-sm">ایجاد تخفیف عمومی</a>
+                    <div class="max-width-16-rem">
+                        <input type="text" class="form-control form-control-sm form-text" placeholder="جستجو">
+                    </div>
+                </section>
+
+                <section class="table-responsive">
+                    <table class="table table-striped table-hover">
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>درصد تخفیف</th>
+                            <th>سقف تخفیف</th>
+                            <th>عنوان مناسبت</th>
+                            <th>تاریخ شروع</th>
+                            <th>تاریخ پایان</th>
+                            <th class="max-width-16-rem text-center"><i class="fa fa-cogs"></i> تنظیمات</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach ($commonDiscounts as $commonDiscount)
+
                             <tr>
-                                <th>#</th>
-                                <th>درصد تخفیف</th>
-                                <th>سقف تخفیف</th>
-                                <th>عنوان کوپن</th>
-                                <th>تاریخ شروع</th>
-                                <th>تاریخ پایان</th>
-                                <th class="max-width-16-rem text-center"><i class="fa fa-cogs"></i> تنظیمات</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <th>1</th>
-                                <td>25%</td>
-                                <td>250000</td>
-                                <td>عمومی</td>
-                                <td>1402/7/1</td>
-                                <td>1402/8/1</td>
+                                <th>{{ $loop->iteration }}</th>
+                                <th>{{ $commonDiscount->percentage }}%</th>
+                                <th>{{ $commonDiscount->discount_ceiling }} تومان</th>
+                                <th>{{ $commonDiscount->title }}</th>
+                                <td>{{ jalaliDate($commonDiscount->start_date) }}</td>
+                                <td>{{ jalaliDate($commonDiscount->end_date) }}</td>
                                 <td class="width-16-rem text-left">
-                                    <a href="#" class="btn btn-sm btn-primary align-items-center"><i
-                                            class="fa fa-edit"></i> ویرایش </a>
-                                    <button class="btn btn-sm btn-danger"><i class="fa fa-trash-alt"></i> حذف </button>
+                                    <a href="{{ route('admin.market.discount.commonDiscount.edit', $commonDiscount->id) }}" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i> ویرایش</a>
+                                    <form class="d-inline" action="{{ route('admin.market.discount.commonDiscount.destroy', $commonDiscount->id) }}" method="post">
+                                        @csrf
+                                        {{ method_field('delete') }}
+                                        <button class="btn btn-danger btn-sm delete" type="submit"><i class="fa fa-trash-alt"></i> حذف</button>
+                                    </form>
                                 </td>
                             </tr>
-                            </tbody>
-                        </table>
-                    </section>
+
+                        @endforeach
+
+                        </tbody>
+                    </table>
                 </section>
+
             </section>
         </section>
     </section>
 
 @endsection
 
+
+@section('script')
+
+    @include('admin.alert.sweatalert.delete-confirm', ['className' => 'delete'])
+
+
+@endsection
