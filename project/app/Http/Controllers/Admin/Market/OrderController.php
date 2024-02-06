fb64b4f682e9c175b628af06094f172dfb683152
Hamed.H <hamedhssnzdh@gmail.com>
2024-02-06 13:46:26 +0330

    write orders code

--- project/app/Http/Controllers/Admin/Market/OrderController.php
+++ project/app/Http/Controllers/Admin/Market/OrderController.php
@@ -8,45 +8,54 @@ use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    public function all()
-    {
-        $orders=Order::all();
-        return view('admin.market.order.index',compact('orders'));
-    }
     public function newOrders()
     {
-        //
+        $orders = Order::where('order_status', 0)->get();
+        foreach ($orders as $order) {
+            $order->order_status = 1;
+            $result = $order->save();
+        }
+        return view('admin.market.order.index', compact('orders'));
     }
     public function sending()
     {
-        //
+        $orders = Order::where('delivery_status', 1)->get();
+        return view('admin.market.order.index', compact('orders'));
     }
     public function unpaid()
     {
-        //
+        $orders = Order::where('payment_status', 0)->get();
+        return view('admin.market.order.index', compact('orders'));
     }
     public function canceled()
     {
-        //
+        $orders = Order::where('order_status', 4)->get();
+        return view('admin.market.order.index', compact('orders'));
     }
     public function returned()
     {
-        //
+        $orders = Order::where('order_status', 5)->get();
+        return view('admin.market.order.index', compact('orders'));
+    }
+    public function all()
+    {
+        $orders = Order::all();
+        return view('admin.market.order.index', compact('orders'));
     }
     public function show()
     {
-        //
+        return view('admin.market.order.index');
     }
     public function changeSendStatus()
     {
-        //
+        return view('admin.market.order.index');
     }
     public function changeOrderStatus()
     {
-        //
+        return view('admin.market.order.index');
     }
     public function cancelOrder()
     {
-        //
+        return view('admin.market.order.index');
     }
 }
